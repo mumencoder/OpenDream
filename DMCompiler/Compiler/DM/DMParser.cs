af0e6732ff73bfacdcd37e5a163010f5e133bac0
mumencoder <mumencoder@users.noreply.github.com>
2021-12-31 16:01:41 -0500

    assorted bugfix

--- DMCompiler/Compiler/DM/DMParser.cs
+++ DMCompiler/Compiler/DM/DMParser.cs
@@ -1937,9 +1937,8 @@ namespace DMCompiler.Compiler.DM {
             return primary;
         }
 
-        static bool ExperimentalParser = false;
         public DMASTExpression Constant() {
-            if (ExperimentalParser) {
+            if (ExperimentalPreproc) {
                 return ExperimentalConstant();
             }
             else {

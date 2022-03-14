0dad3681e559bf10648e4889e83b7bd90920e7f3
mumencoder <mumencoder@users.noreply.github.com>
2022-03-13 21:47:58 -0400

    add define visitor

---
+++ DMCompiler/DM/Visitors/DMDefineVisitor.cs
@@ -0,0 +1,46 @@
+﻿
+using DMCompiler.Compiler.DM;
+using System;
+
+namespace DMCompiler.DM.Visitors {
+    public class DMDefineVisitor : DMASTVisitor {
+        Action<DMASTNode> Handler;
+
+        public DMDefineVisitor(Action<DMASTNode> handler) {
+            Handler = handler;
+        }
+
+        public void VisitFile(DMASTFile dmFile) {
+            dmFile.BlockInner.Visit(this);
+        }
+
+        public void VisitBlockInner(DMASTBlockInner blockInner) {
+            foreach (DMASTStatement statement in blockInner.Statements) {
+                statement.Visit(this);
+            }
+        }
+
+        public void VisitObjectDefinition(DMASTObjectDefinition statement) {
+            Handler(statement);
+            statement.InnerBlock?.Visit(this);
+        }
+
+        public void VisitObjectVarDefinition(DMASTObjectVarDefinition objectVarDefinition) {
+            Handler(objectVarDefinition);
+        }
+
+        public void VisitMultipleObjectVarDefinitions(DMASTMultipleObjectVarDefinitions multipleObjectVarDefinitions) {
+            foreach (DMASTObjectVarDefinition varDefinition in multipleObjectVarDefinitions.VarDefinitions) {
+                varDefinition.Visit(this);
+            }
+        }
+
+        public void VisitObjectVarOverride(DMASTObjectVarOverride objectVarOverride) {
+            Handler(objectVarOverride);
+        }
+
+        public void VisitProcDefinition(DMASTProcDefinition procDefinition) {
+            Handler(procDefinition);
+        }
+    }
+}
